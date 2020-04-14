3b0582d0830b309cd970b666dcc04b060e019bd7
may <bingqing0822@163.com>
2020-04-14 23:45:22 +0800

    chapter 3

--- c++/book/chapter3/3_2.cpp
+++ c++/book/chapter3/3_2.cpp
@@ -6,10 +6,33 @@
 #include <iostream>
 using namespace std;
 
-void main()
+// 声明函数
+double power(double x, int n);
+
+int main()
 {
+    int value = 0;
 
-
+    cout<<"Enter an 8 bit binary number: ";
+    for (int i = 7; i >= 0; i--)
+    {
+        char ch;
+        cin>>ch;
+        if (ch == '1')
+        {
+            value += power(2, i);
+        }
+    }
+    cout<<"Decimal value is "<<value<<endl;
+    return 0;
 }
 
+double power(double x, int n){
+    double val = 1.0;
+    while (n--)
+    {
+        val *= x;
+    }
+    return val;
+}
 
