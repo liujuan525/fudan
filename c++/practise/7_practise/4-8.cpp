c8972fb6ecab2a7ee554a5e3eb22cf1b4e7f5aad
may <bingqing0822@163.com>
2020-06-02 18:19:01 +0800

    practise 7
    practise 7

--- c++/practise/7_practise/4-8.cpp
+++ c++/practise/7_practise/4-8.cpp
@@ -4,3 +4,45 @@
  * @Date: 2020-05-24 18:36:22
  */ 
 #include <iostream>
+using namespace std;
+
+class Dog
+{
+    public:
+        Dog (int initialAge = 0, int initialWeight = 5);
+        ~Dog();
+        int GetAge() { return itsAge;} // inline!
+        void SetAge (int age) { itsAge = age;} // inline!
+        int GetWeight() { return itsWeight;} // inline!
+        void SetWeight (int weight) { itsWeight = weight;} // inline!
+    private:
+        int itsAge, itsWeight;
+};
+
+Dog::Dog(int initialAge, int initialWeight)
+{
+    itsAge = initialAge;
+    itsWeight = initialWeight;
+}
+
+Dog::~Dog() //destructor， takes no action
+{
+
+}
+
+int main()
+{
+    Dog Jack(2, 10);
+    cout << "Jack is a Dog who is " ;
+    cout << Jack.GetAge() << " years old and ";
+    cout << Jack.GetWeight() << " pounds weight.\n";
+
+    Jack.SetAge(7);
+    Jack.SetWeight(20);
+    cout << "Now Jack is " ;
+    cout << Jack.GetAge() << " years old and ";
+    cout << Jack.GetWeight() << " pounds weight.";
+
+    return 0;
+}
+
--- c++/practise/7_practise/4-9.cpp
+++ c++/practise/7_practise/4-9.cpp
@@ -4,3 +4,51 @@
  * @Date: 2020-05-24 18:36:30
  */ 
 #include <iostream>
+using namespace std;
+
+class Rectangle
+{
+    public:
+        Rectangle (int top, int left, int bottom, int right);
+        ~Rectangle () {}
+        int GetTop() const { return itsTop; }
+        int GetLeft() const { return itsLeft; }
+        int GetBottom() const { return itsBottom; }
+        int GetRight() const { return itsRight; }
+        void SetTop(int top) { itsTop = top; }
+        void SetLeft (int left) { itsLeft = left; }
+        void SetBottom (int bottom) { itsBottom = bottom; }
+        void SetRight (int right) { itsRight = right; }
+        int GetArea() const;
+    private:
+        int itsTop;
+        int itsLeft;
+        int itsBottom;
+        int itsRight;
+};
+
+Rectangle::Rectangle(int top, int left, int bottom, int right)
+{
+    itsTop = top;
+    itsLeft = left;
+    itsBottom = bottom;
+    itsRight = right;
+}
+
+int Rectangle::GetArea() const
+{
+    int Width = itsRight-itsLeft;
+    int Height = itsTop - itsBottom;
+    return (Width * Height);
+}
+
+int main()
+{
+    Rectangle MyRectangle (100, 20, 50, 80 );
+    int Area = MyRectangle.GetArea();
+    cout << "Area: " << Area << "\n";
+
+    return 0;
+}
+
+
