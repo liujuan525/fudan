175260876dabe60c78ffba43404372469616b5c3
May <bingqing0822@163.com>
2020-06-02 14:00:41 +0800

    Update 3-12.cpp
    practise 5

--- c++/practise/5_practise/3-12.cpp
+++ c++/practise/5_practise/3-12.cpp
@@ -4,3 +4,25 @@
  * @Date: 2020-05-31 19:42:12
  */ 
 #include <iostream>
+using namespace std;
+
+// 递归计算
+int calculate(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cout << "请输入一个整数：" ;
+    cin >> n;
+    cout << "递归结果为：" << calculate(n) << endl;
+
+    return 0;
+}
