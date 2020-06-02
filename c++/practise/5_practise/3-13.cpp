9542a2ad9790039773d65336850d1842e205340b
May <bingqing0822@163.com>
2020-06-02 14:02:32 +0800

    Update 3-13.cpp

--- c++/practise/5_practise/3-13.cpp
+++ c++/practise/5_practise/3-13.cpp
@@ -5,4 +5,27 @@
  * @Author: may
  * @Date: 2020-05-31 19:42:21
  */ 
-// 
+#include <iostream>
+using namespace std;
+
+// 斐波那契函数
+int fibonacci(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    } else
+    {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "请输入一个整数：";
+    cin >> n;
+    cout << "计算结果为：" << fibonacci(n) << endl;
+
+    return 0;
+}
