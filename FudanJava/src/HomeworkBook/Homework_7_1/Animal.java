93483a7e98bbcb607d1c3a6435d830825c843496
may <bingqing0822@163.com>
2021-05-18 15:03:01 +0800

    homework book

---
+++ FudanJava/src/HomeworkBook/Homework_7_1/Animal.java
@@ -0,0 +1,10 @@
+package HomeworkBook.Homework_7_1;
+
+public class Animal {
+    private double weight;
+
+    public void eat(){
+        System.out.println("I can eat anything.");
+    }
+}
+
---
+++ FudanJava/src/HomeworkBook/Homework_7_1/Bird.java
@@ -0,0 +1,9 @@
+package HomeworkBook.Homework_7_1;
+
+public class Bird extends Animal{
+    public int numberOfWings;
+
+    public void fly(){
+        System.out.println("I can fly." );
+    }
+}
